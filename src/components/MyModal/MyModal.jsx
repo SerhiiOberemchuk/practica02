2f9ed8ef161da6fc792e02047c217284b662bb78
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-12-23 17:14:28 +0200

    commit

---
+++ src/components/MyModal/MyModal.jsx
@@ -0,0 +1,29 @@
+import Modal from 'react-modal';
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    backgroundColor: 'grey',
+  },
+};
+
+Modal.setAppElement('#root');
+
+export const MyModal = ({ modalIsOpen, closeModal, largeUrl, alt }) => {
+  return (
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <img src={largeUrl} alt={alt} />
+    </Modal>
+  );
+};
